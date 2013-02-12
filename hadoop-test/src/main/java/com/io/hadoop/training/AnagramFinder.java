5945f6557992ea48eb41c4777f841163efe83bfa
Jos Boumans <jos@dwim.org>
2013-02-11 17:35:24 -0800

    * implement anagram finding

--- hadoop-test/src/main/java/com/io/hadoop/training/AnagramFinder.java
+++ hadoop-test/src/main/java/com/io/hadoop/training/AnagramFinder.java
@@ -1,11 +1,8 @@
 package com.io.hadoop.training;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -25,6 +22,7 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 
+import org.apache.commons.lang.StringUtils;
 
 public class AnagramFinder extends Configured implements Tool {
 
@@ -66,6 +64,7 @@ public class AnagramFinder extends Configured implements Tool {
     public  static  class   AnagramFinderMR
             implements  Mapper<LongWritable ,Text ,Text ,Text>
             ,Reducer<Text ,Text  ,Text ,NullWritable> {
+
         public void configure(JobConf jc) {
 
         }
@@ -78,13 +77,65 @@ public class AnagramFinder extends Configured implements Tool {
 
         public void map(LongWritable key, Text row, OutputCollector<Text, Text> output, Reporter reporter)
                 throws IOException {
-            // TODO IMPLEMENT THIS
+
+            // Following: http://wiki.apache.org/hadoop/WordCount
+            String line = row.toString();
+
+            //System.out.println( "Input: " + line );
+
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                Text word   = new Text( tokenizer.nextToken( ) );
+
+                // Following: http://stackoverflow.com/questions/605891/sort-a-single-string-in-java
+                char[] chars = word.toString().toCharArray();
+                Arrays.sort( chars );
+
+                // Hadoop insists on using the Text type for input/output, not string
+                Text sorted = new Text( new String(chars) );
+
+                //System.out.println( "Word: " + word );
+                //System.out.println( "Key: " + sorted );
+
+                output.collect( sorted, word );
+            }
+
+
+
         }
 
         public void reduce(Text key, Iterator<Text> iterator, OutputCollector<Text, NullWritable> output, Reporter reporter)
                 throws IOException {
+            // Key: ascii-sorted word
+            // Value: words
 
-            // TODO IMPLEMENT THIS
+            // Arrays are a fixed size.. Since we don't know the amount of values (nor should we care), it's
+            // not suitable. There's a List implementation that does grow, but it's apparently not part of the
+            // standard library..
+            // String[] words;
+            // Instead, use ArrayList, which can be extended:
+            // http://docs.oracle.com/javase/6/docs/api/java/util/ArrayList.html
+            List<String> words = new ArrayList<String>();
+
+
+
+            // So.. Java doesn't have an Array.join() method!?
+            // http://stackoverflow.com/questions/1978933/a-quick-and-easy-way-to-join-array-elements-with-a-separator-the-oposite-of-spl
+            // Write a ghetto version ourselves instead then...
+            int _seenFirstElement = 0;
+
+            while (iterator.hasNext()) {
+
+                // Only skip this on the first element we're adding, meaning all subsequent invocations
+                // add the '^' first, then their word.
+                // Also, ++_seenFirstElement doesn't work either.. and this HAS to be a boolean, not an int...
+                // DWIM already...
+
+                words.add( iterator.next().toString() );
+            }
+
+
+            output.collect(new Text( StringUtils.join(words, "^") ), NullWritable.get());
         }
     }
 
